6b1900a2905b21eb16ad5c44e8e2d9a83bf2e531
Paulo Lopes <silvavitorlopes711@gmail.com>
2020-04-06 17:07:22 -0300

    chore: initial commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const modalOverlay = document.querySelector('.modal-overlay');
+const cards = document.querySelectorAll('.card');
+
+for (let card of cards){
+    card.addEventListener("click", function(){
+        const siteUrl = card.getAttribute('id');
+        modalOverlay.classList.add('active')
+        modalOverlay.querySelector("iframe").src=`https://rocketseat.com.br/${siteUrl}`
+    });
+}
+
+document.querySelector('.close-modal').addEventListener("click", function(){
+    modalOverlay.classList.remove('active')
+    modalOverlay.querySelector("iframe").src=""
+});
